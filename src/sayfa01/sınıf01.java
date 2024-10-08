e5c99e6b541efc58d14c16dfebc446ac79073c82
Döne Güler <doneguler173@gmail.com>
2024-10-08 21:42:13 +0300

    yan dal güncellemesi commitlenip push yapılacak

--- src/sayfa01/sınıf01.java
+++ src/sayfa01/sınıf01.java
@@ -3,5 +3,6 @@ package sayfa01;
 public class sınıf01 {
     public static void main(String[] args) {
         System.out.println("döne yeniden denedi");
+        System.out.println("yan dal brancına ekleme yapıldı");
     }
 }
